1d8202e93b2af7bfe886d9110a72b866b62b4ba2
evorick <rickkadlac@gmail.com>
2015-09-28 08:46:16 -0400

    added more buyer model level validations

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -5,5 +5,7 @@ class Buyer < ActiveRecord::Base
   validates :phone_number, :number_bedrooms, :number_bathrooms, :min_price, :max_price, numericality: { only_integer: true }
   validates :number_bedrooms, :number_bathrooms, numericality: { greater_than_or_equal_to: 1 }
   validates :min_price, numericality: { greater_than_or_equal_to: 0 }
-  validates :max_price, numericality: { greater_than_or_equal_to: 50000 }
+  validates :max_price, numericality: { greater_than_or_equal_to: 10000 }
+  validates :email, format: { with: /.+@.+\..+/i, message: "must be a valid email address" }
+  validates :email, uniqueness: true
 end
