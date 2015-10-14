adb39e2464de751e5952b9cba239a56cb94b5e2b
evorick <rickkadlac@gmail.com>
2015-10-14 15:48:04 -0400

    added more bootstrap styling to the buyers search page

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'buyers#search'
+  root 'listers#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
